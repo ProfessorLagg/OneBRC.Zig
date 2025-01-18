f9399d88efff131d84b2b445c5d030ebcdbde01c
ProfessorLagg <futurecake2999@gmail.com>
2025-01-18 15:18:52 +0100

    Removed lib.zig

--- src/cli.zig
+++ src/cli.zig
@@ -4,11 +4,7 @@ const Allocator = std.mem.Allocator;
 const GeneralPurposeAllocator = std.heap.GeneralPurposeAllocator;
 
 const parsing = @import("parsing.zig");
-const lib = @import("lib.zig");
-const SetParser = lib.SetParser;
-
 const parallel = @import("parallel/parallel.zig");
-
 const sorted = @import("sorted/sorted.zig");
 
 pub const std_options = .{
@@ -21,14 +17,7 @@ pub const std_options = .{
     },
 };
 
-// TODO go through and set every parameter of the GeneralPurposeAllocator.Config
-var gpa: GeneralPurposeAllocator(.{}) = GeneralPurposeAllocator(.{}){};
-var main_allocator: Allocator = undefined;
-var parser: SetParser = undefined;
 pub fn main() !void {
-    try init();
-    defer deinit();
-
     // TODO parse console args
     // const path = "C:\\CodeProjects\\1BillionRowChallenge\\data\\verysmall.txt";
     //const path = "C:\\CodeProjects\\1BillionRowChallenge\\data\\small.txt";
@@ -48,18 +37,4 @@ pub fn main() !void {
     const keyCount_f64: f64 = @floatFromInt(uniqueKeys);
     const key_percent: f64 = (keyCount_f64 / lineCount_f64) * 100;
     std.log.warn("read {d:.0} lines in {d:.2} s | {d:.2} ns/line | found {d} unique keys ({d:.2}%)", .{ parseResult.lineCount, s, ns_per_line, uniqueKeys, key_percent });
-}
-
-fn init() !void {
-    main_allocator = gpa.allocator();
-    parser = SetParser.init(main_allocator);
-}
-fn deinit() void {
-    parser.deinit();
-
-    const gpa_deinit_status = gpa.deinit();
-    switch (gpa_deinit_status) {
-        .ok => std.log.debug("GeneralPurposeAllocator deninit status: OK", .{}),
-        .leak => std.log.warn("GeneralPurposeAllocator deninit status: LEAK", .{}),
-    }
 }
