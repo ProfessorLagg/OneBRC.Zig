b5470a8147e42475f35e428f1e83246b30fbaabf
ProfessorLagg <futurecake2999@gmail.com>
2025-01-22 15:05:53 +0100

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -2,7 +2,7 @@
 cd %~dp0
 SET ExePath=.\zig-out\bin\1brc.cli.exe
 :: Based on the least active cores on my specific PC. You should change this
-SET AFFINITY=0x0000000000002FA8
+SET AFFINITY=0x0000000000000200
 
 if exist %ExePath% del %ExePath%
 zig build -Doptimize=ReleaseFast -freference-trace
