fb8b3d8dd34dd7e01e671663f206169251960bb9
ProfessorLagg <futurecake2999@gmail.com>
2025-01-23 17:57:00 +0100

    marge

--- src/cli.zig
+++ src/cli.zig
@@ -19,9 +19,9 @@ pub const std_options = .{
 
 // const path = "C:\\CodeProjects\\1BillionRowChallenge\\data\\verysmall.txt";
 // const path = "C:\\CodeProjects\\1BillionRowChallenge\\data\\small.txt";
-// const path = "C:\\CodeProjects\\1BillionRowChallenge\\data\\medium.txt";
+const path = "C:\\CodeProjects\\1BillionRowChallenge\\data\\medium.txt";
 // const path = "C:\\CodeProjects\\1BillionRowChallenge\\data\\1GB.txt";
-const path = "C:\\CodeProjects\\1BillionRowChallenge\\data\\large.txt";
+// const path = "C:\\CodeProjects\\1BillionRowChallenge\\data\\large.txt";
 
 pub fn main() !void {
     // TODO parse console args
@@ -43,7 +43,6 @@ fn run_debug() !void {
     const s: f64 = ns / std.time.ns_per_s;
     const ns_per_line: f64 = ns / @as(f64, @floatFromInt(parseResult.lineCount));
 
-    // Turn this into a cli argument
     const lineCount_f64: f64 = @floatFromInt(parseResult.lineCount);
     const uniqueKeys = parseResult.uniqueKeys;
     const keyCount_f64: f64 = @floatFromInt(uniqueKeys);
@@ -53,18 +52,33 @@ fn run_debug() !void {
 
 fn run_read() !void {
     var timer = try std.time.Timer.start();
-    const parseResult: parsing.ParseResult = try parsing.read(path[0..]);
+    const parseResult: parsing.ParseResult = try parsing.parse(path[0..], false);
 
     const ns: f64 = @floatFromInt(timer.read());
     const s: f64 = ns / std.time.ns_per_s;
     const ns_per_line: f64 = ns / @as(f64, @floatFromInt(parseResult.lineCount));
 
-    // Turn this into a cli argument
     const lineCount_f64: f64 = @floatFromInt(parseResult.lineCount);
     const uniqueKeys = parseResult.uniqueKeys;
     const keyCount_f64: f64 = @floatFromInt(uniqueKeys);
     const key_percent: f64 = (keyCount_f64 / lineCount_f64) * 100;
-    std.log.warn("read {d:.0} lines in {d:.2} s | {d:.2} ns/line | found {d} unique keys ({d:.2}%)", .{ parseResult.lineCount, s, ns_per_line, uniqueKeys, key_percent });
+
+    const stat: std.fs.File.Stat = blk: {
+        var buf: [4096]u8 = undefined;
+        const abspath = try std.fs.cwd().realpath(path, buf[0..]);
+        var file = try std.fs.openFileAbsolute(abspath, comptime std.fs.File.OpenFlags{ //NOFOLD
+            .mode = .read_only,
+            .lock = .none,
+            .lock_nonblocking = false,
+            .allow_ctty = false,
+        });
+        defer file.close();
+        const stat = try file.stat();
+        break :blk stat;
+    };
+
+    const bytes_per_second: u64 = @intFromFloat(@as(f64, @floatFromInt(stat.size)) / s);
+    std.log.warn("read {d:.0} lines in {d:.2} s | {d:.2} ns/line | found {d} unique keys ({d:.2}%) | read speed: {d:.3}/s", .{ parseResult.lineCount, s, ns_per_line, uniqueKeys, key_percent, std.fmt.fmtIntSizeBin(bytes_per_second) });
 }
 
 fn run_benchmark() void {
