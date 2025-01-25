631c69c258f8027e22c52cb9d25e333afd6e3096
ProfessorLagg <futurecake2999@gmail.com>
2025-01-25 01:16:34 +0100

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -3,11 +3,10 @@ cd %~dp0
 SET ExePath=.\zig-out\bin\1brc.cli.exe
 :: Based on the least active cores on my specific PC. You should change this
 SET AFFINITY=0x0000000000000200
-SET NUMA=1
 if exist %ExePath% del %ExePath%
 zig build -Doptimize=ReleaseFast -freference-trace
 if exist %ExePath% (
     cls
-    start /B /WAIT /HIGH /NODE %NUMA% /AFFINITY %AFFINITY% "OneBillionRows" %ExePath%
+    start /B /WAIT /HIGH /AFFINITY %AFFINITY% "OneBillionRows" %ExePath%
     pause
 )
