d7812c5d0a478a8f379a3ada7a6c140aab07b81c
ProfessorLagg <futurecake2999@gmail.com>
2025-08-25 19:05:22 +0200

    Cleaned out old files

--- src/root.zig
+++ src/root.zig
@@ -1,42 +1,9 @@
-pub const utils = @import("utils.zig");
-
-pub const DelimReader = @import("delimReader.zig");
-
-const vecstr = @import("vecstr.zig");
-
 pub const _asm = @import("_asm.zig");
-
-const BRCBlockReader_ns = @import("BRCBlockReader.zig");
-pub const BRCBlockReader = BRCBlockReader_ns.BRCBlockReader;
-pub const BRCBlockReaderUnmanaged = BRCBlockReader_ns.BRCBlockReaderUnmanaged;
-
-pub const BRCParser = @import("BRCParser.zig");
-
-pub const DynamicBuffer = @import("DynamicBuffer.zig");
-
-pub const DynamicArray = @import("DynamicArray.zig");
-
-test utils {
-    _ = utils;
-}
-test DelimReader {
-    _ = DelimReader;
-}
-test vecstr {
-    _ = vecstr;
-}
 test _asm {
     _ = _asm;
 }
-test BRCBlockReader {
-    _ = BRCBlockReader;
-}
-test BRCParser {
-    _ = BRCParser;
-}
-test DynamicBuffer {
-    _ = DynamicBuffer;
-}
-test DynamicArray {
-    _ = DynamicArray;
+
+pub const fileMapping = @import("fileMapping.zig");
+test fileMapping {
+    _ = fileMapping;
 }
