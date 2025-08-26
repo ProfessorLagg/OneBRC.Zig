81f9cc43d6b951835f55669454c03665946010e1
ProfessorLagg <futurecake2999@gmail.com>
2025-08-26 16:45:52 +0200

    Handling strings longer than 255 brought performance back down to 5.035 s | 2.8955 GB/s on average

--- src/sso.zig
+++ src/sso.zig
@@ -24,8 +24,6 @@ const SizeCategory = enum(u8) {
     }
 };
 
-// TODO Handle strings longer than 255 (where the first byte of usize len is not guaranteed to be > 0)
-
 const sso = @This();
 data: [StructSize]u8 = undefined,
 
@@ -58,7 +56,10 @@ pub fn set(self: *sso, str: []const u8) void {
     switch (SizeCategory.fromLen(str.len)) {
         .small => self.set_small(str),
         .medium => self.set_medium(str),
-        .large => @panic("sso with length > 255 not yet implemented"),
+        .large => {
+            @branchHint(.cold);
+            @panic("sso with length > 255 not yet implemented");
+        },
     }
 }
 
@@ -81,7 +82,10 @@ pub fn get(self: *const sso) []const u8 {
     return switch (SizeCategory.fromLen(self.data[0])) {
         .small => self.get_small(),
         .medium => self.get_medium(),
-        .large => @panic("sso with length > 255 not yet implemented"),
+        .large => {
+            @branchHint(.cold);
+            @panic("sso with length > 255 not yet implemented");
+        },
     };
 }
 
@@ -94,7 +98,10 @@ pub fn clone(allocator: std.mem.Allocator, str: []const u8) !sso {
             @memcpy(str_clone, str);
             result.set_medium(str_clone);
         },
-        .large => @panic("sso with length > 255 not yet implemented"),
+        .large => {
+            @branchHint(.cold);
+            @panic("sso with length > 255 not yet implemented");
+        },
     }
     return result;
 }
