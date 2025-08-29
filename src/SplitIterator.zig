b96a01100fb0de33f0b1dabbf1cccd3b822b9aa1
ProfessorLagg <futurecake2999@gmail.com>
2025-08-29 23:28:23 +0200

    Begun building custom indexOf delim finder for split iterator. It's not quite there yet. Perf is 4.389 s | 3.3221 GB/s
    Times:
            4.505 s | 3.2362 GB/s
            4.305 s | 3.3863 GB/s
            4.513 s | 3.2302 GB/s
            4.334 s | 3.3637 GB/s
            4.326 s | 3.3700 GB/s
    Best : 4.305 s | 3.3863 GB/s
    Mean : 4.397 s | 3.3158 GB/s
    Worst: 4.513 s | 3.2302 GB/s
    BRC  : 4.389 s | 3.3221 GB/s

--- src/SplitIterator.zig
+++ src/SplitIterator.zig
@@ -1,14 +1,51 @@
 const builtin = @import("builtin");
 const std = @import("std");
 
+const backend_supports_vectors = switch (builtin.zig_backend) {
+    .stage2_llvm, .stage2_c => true,
+    else => false,
+};
+
 pub fn SplitIterator(comptime delimiter: u8) type {
     return struct {
         const Self = @This();
         buffer: []const u8,
+
+        fn indexOfDelim(slice: []const u8) ?usize {
+            @setRuntimeSafety(false);
+
+            var i: usize = 0;
+            const Vec32 = @Vector(32, u8);
+            const mask32: Vec32 = comptime (@as(Vec32, @splat(delimiter)));
+            while ((slice.len - i) <= 32) {
+                const block: *align(1) const Vec32 = @ptrCast(slice[i..][0..32]);
+                const matches = block.* == mask32;
+                if (@call(.always_inline, std.simd.firstTrue, .{matches})) |I| {
+                    return i + I;
+                }
+                i += 32;
+            }
+
+            const Vec16 = @Vector(16, u8);
+            const mask16: Vec16 = comptime (@as(Vec16, @splat(delimiter)));
+            while ((slice.len - i) <= 16) {
+                const block: *align(1) const Vec16 = @ptrCast(slice[i..][0..16]);
+                const matches = block.* == mask16;
+                if (@call(.always_inline, std.simd.firstTrue, .{matches})) |I| {
+                    return i + I;
+                }
+                i += 16;
+            }
+
+            for (slice[i..], i..) |c, j| if (c == delimiter) return j;
+            return null;
+        }
+
         pub fn next(self: *Self) ?[]const u8 {
             if (self.buffer.len == 0) return null;
             // TODO i can optimize this better because i know the delimiter at comptime
-            const si: usize = std.mem.indexOfScalar(u8, self.buffer, delimiter) orelse self.buffer.len;
+            // const si: usize = std.mem.indexOfScalar(u8, self.buffer, delimiter) orelse self.buffer.len;
+            const si: usize = indexOfDelim(self.buffer) orelse self.buffer.len;
             const result: []const u8 = self.buffer[0..si];
             self.buffer = self.buffer[@min(self.buffer.len, si + 1)..];
             return result;
