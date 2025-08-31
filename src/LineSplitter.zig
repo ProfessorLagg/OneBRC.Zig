1063ba8e13902b37dd9efb8172e4c9238bca6854
ProfessorLagg <futurecake2999@gmail.com>
2025-08-31 18:48:35 +0200

    Made LineSplitter.indexOfDelim actually faster by copying std.mem.indexOfScalarPos, setting start index to 5 and block_len to 16. Perf is 3.873 s | 3.7644 GB/s
    Times:
            3.909 s | 3.7301 GB/s
            3.842 s | 3.7944 GB/s
            3.843 s | 3.7939 GB/s
            3.954 s | 3.6874 GB/s
            3.867 s | 3.7699 GB/s
    Best : 3.842 s | 3.7944 GB/s
    Mean : 3.883 s | 3.7547 GB/s
    Worst: 3.954 s | 3.6874 GB/s
    BRC  : 3.873 s | 3.7644 GB/s

--- src/LineSplitter.zig
+++ src/LineSplitter.zig
@@ -9,18 +9,59 @@ const backend_supports_vectors = switch (builtin.zig_backend) {
 pub const LineSplitter = @This();
 buffer: []const u8,
 
-inline fn indexOfDelim(self: *const LineSplitter) usize {
+fn indexOfDelim(self: *const LineSplitter) ?usize {
     @setRuntimeSafety(false);
-    const buf: []const u8 = self.buffer[5..];
-    const idx: usize = std.mem.indexOfScalar(u8, buf, '\n') orelse buf.len;
-    return idx + 5;
+    std.debug.assert(backend_supports_vectors);
+    std.debug.assert(!std.debug.inValgrind());
+    std.debug.assert(!@inComptime());
+    const block_len = 16;
+    const slice: []const u8 = self.buffer[0..];
+    var i: usize = 5;
+    const Block: type = @Vector(block_len, u8);
+    if (i + 2 * block_len < slice.len) {
+        const mask: Block = @splat('\n');
+        while (true) {
+            inline for (0..2) |_| {
+                const block: Block = slice[i..][0..block_len].*;
+                const matches = block == mask;
+                if (@reduce(.Or, matches)) {
+                    return i + std.simd.firstTrue(matches).?;
+                }
+                // if (std.simd.firstTrue(matches)) |I| return i + I;
+                i += block_len;
+            }
+            if (i + 2 * block_len >= slice.len) break;
+        }
+    }
+    
+    inline for (0..2) |j| {
+        const block_x_len = block_len / (1 << j);
+        comptime if (block_x_len < 4) break;
+
+        const BlockX = @Vector(block_x_len, u8);
+        if (i + block_x_len < slice.len) {
+            const mask: BlockX = @splat('\n');
+            const block: BlockX = slice[i..][0..block_x_len].*;
+            const matches = block == mask;
+            if (@reduce(.Or, matches)) {
+                return i + std.simd.firstTrue(matches).?;
+            }
+            // if (std.simd.firstTrue(matches)) |I| return i + I;
+            i += block_x_len;
+        }
+    }
+
+    for (slice[i..], i..) |c, j| {
+        if (c == '\n') return j;
+    }
+    return null;
 }
 pub fn next(self: *LineSplitter) ?[]const u8 {
     if (self.buffer.len == 0) return null;
     // TODO i can optimize this better because i know the delimiter at comptime
-    const si: usize = self.indexOfDelim();
+    // const si: usize = std.mem.indexOfScalarPos(u8, self.buffer, 5, '\n') orelse self.buffer.len;
+    const si: usize = self.indexOfDelim() orelse self.buffer.len;
     const result: []const u8 = self.buffer[0..si];
-    // TODO i can probably set the result to more than si + 1 since i know theres some minimum amount of bytes before the next \n
     self.buffer = self.buffer[@min(self.buffer.len, si + 1)..];
     return result;
 }
