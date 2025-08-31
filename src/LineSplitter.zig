57b71dd5d6929ef44e3fecb9433af361a80a7c0c
ProfessorLagg <futurecake2999@gmail.com>
2025-08-31 18:25:52 +0200

    Updated LineSplitter with custom IndexOfDelim method. Perf is 4.028 s | 3.6191 GB/s (which is not better)
    Times:
            3.965 s | 3.6766 GB/s
            4.362 s | 3.3425 GB/s
            3.954 s | 3.6873 GB/s
            4.054 s | 3.5960 GB/s
            4.066 s | 3.5861 GB/s
    Best : 3.954 s | 3.6873 GB/s
    Mean : 4.080 s | 3.5732 GB/s
    Worst: 4.362 s | 3.3425 GB/s
    BRC  : 4.028 s | 3.6191 GB/s

--- src/LineSplitter.zig
+++ src/LineSplitter.zig
@@ -8,10 +8,17 @@ const backend_supports_vectors = switch (builtin.zig_backend) {
 
 pub const LineSplitter = @This();
 buffer: []const u8,
+
+inline fn indexOfDelim(self: *const LineSplitter) usize {
+    @setRuntimeSafety(false);
+    const buf: []const u8 = self.buffer[5..];
+    const idx: usize = std.mem.indexOfScalar(u8, buf, '\n') orelse buf.len;
+    return idx + 5;
+}
 pub fn next(self: *LineSplitter) ?[]const u8 {
     if (self.buffer.len == 0) return null;
     // TODO i can optimize this better because i know the delimiter at comptime
-    const si: usize = std.mem.indexOfScalar(u8, self.buffer, '\n') orelse self.buffer.len;
+    const si: usize = self.indexOfDelim();
     const result: []const u8 = self.buffer[0..si];
     // TODO i can probably set the result to more than si + 1 since i know theres some minimum amount of bytes before the next \n
     self.buffer = self.buffer[@min(self.buffer.len, si + 1)..];
